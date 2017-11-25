3499753d1f15f5169091df60619f71807f4362eb
Joaquín Gamboa Figueroa <joaquin.gamboaf@gmail.com>
2017-11-25 00:41:19 -0300

    Datatables
    agregando datatables según guia
    https://github.com/yajra/laravel-datatables

--- app/config/app.php
+++ app/config/app.php
@@ -184,6 +184,7 @@ return [
         \InfyOm\Generator\InfyOmGeneratorServiceProvider::class,
         \InfyOm\AdminLTETemplates\AdminLTETemplatesServiceProvider::class,
         \InfyOm\GeneratorBuilder\GeneratorBuilderServiceProvider::class,
+        Yajra\DataTables\DataTablesServiceProvider::class,
         /* CAMBIOS INFYOM */
 
     ],
@@ -239,6 +240,7 @@ return [
         'Form'      => Collective\Html\FormFacade::class,
         'Html'      => Collective\Html\HtmlFacade::class,
         'Flash'     => Laracasts\Flash\Flash::class,
+        'DataTables' => Yajra\DataTables\Facades\DataTables::class,
         /* CAMBIOS INFYOM */
 
     ],
